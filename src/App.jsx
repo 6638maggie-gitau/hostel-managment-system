321ca317083c06b518bba3d0e0fae1162bf19251
Margaret Gitau <6638maggie@gmail.com>
2026-03-16 21:24:44 +0300

    Fix router for GitHub pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import Nav from "./componets/Nav";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { HashRouter as Router, Routes, Route} from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
